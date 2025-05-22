a25db307611f6f362404057fe42201b6948636a3
Haarii24 <hafizaansyari@gmai.com>
2025-05-22 20:20:40 +0800

    Initial commit

--- app/Models/KolHubungan.php
+++ app/Models/KolHubungan.php
@@ -25,14 +25,3 @@ class KolHubungan extends Model
     ];
 }
 
-<?php
-class KolHubungan extends Model
-{
-    protected $table = 'kol_hubungan';
-    protected $primaryKey = 'id_hubungan';
-    public $incrementing = false;
-    public $timestamps = false; // tambahkan ini jika tidak ada created_at/updated_at
-    protected $fillable = [
-        'id_hubungan', 'nama_hubungan'
-    ];
-}
