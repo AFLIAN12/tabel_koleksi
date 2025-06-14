1a7a8635581130b045c801482f6355c078ef54ae
AFLIAN12 <afliansyah11@gmail.com>
2025-06-14 16:07:47 +0800

    edit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/docs/api', function () {
+    return view('swagger.index'); // atau sesuai view milikmu
+});
